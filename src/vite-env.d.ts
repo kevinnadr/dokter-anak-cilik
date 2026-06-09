f9d6d902192c64e383eee85517b92dee534a7146
Weellyyy <wildanprtm2304@gmail.com>
2026-06-09 10:59:18 +0700

    menambah  video

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -12,3 +12,13 @@ declare module '*.svg' {
   const content: string;
   export default content;
 }
+
+declare module '*.mp4' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.webm' {
+  const content: string;
+  export default content;
+}
